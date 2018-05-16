d6342252207a54de542547a1a3a1ed1d0a3d9aa6
Eslam El-Meniawy <eslam.elmeniawy@gmail.com>
2018-05-16 11:33:40 +0200

    Add butttons events

--- app/src/main/java/elmeniawy/eslam/tictactoe/MainActivity.kt
+++ app/src/main/java/elmeniawy/eslam/tictactoe/MainActivity.kt
@@ -2,11 +2,33 @@ package elmeniawy.eslam.tictactoe
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun btClicked(view: View) {
+        val btSelected: Button = view as Button
+        var cellId = 0
+
+        when (btSelected.id) {
+            R.id.bt1 -> cellId = 1
+            R.id.bt2 -> cellId = 2
+            R.id.bt3 -> cellId = 3
+            R.id.bt4 -> cellId = 4
+            R.id.bt5 -> cellId = 5
+            R.id.bt6 -> cellId = 6
+            R.id.bt7 -> cellId = 7
+            R.id.bt8 -> cellId = 8
+            R.id.bt9 -> cellId = 9
+        }
+
+        Toast.makeText(this, "ID: $cellId", Toast.LENGTH_SHORT).show()
     }
 }
